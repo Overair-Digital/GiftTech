a23583db4f5f16d7d6b41d66038e3913dd589523
Basker12 <kacperileon@gmail.com>
2022-06-29 12:29:34 +0100

    [Addition] Fixed the cards not adapting entirely correctly.

--- app.js
+++ app.js
@@ -11,23 +11,31 @@ function stickyNav() { // nice stolen code
     }
 }
 
-document.addEventListener("DOMContentLoaded", () => {
-    function counter(id, start, end, duration) {
-        let obj = document.getElementById(id),
-            current = start,
-            range = end - start,
-            increment = end > start ? 1 : -1,
-            step = Math.abs(Math.floor(duration / range)),
-            timer = setInterval(() => {
-                current += increment;
-                obj.textContent = current;
-                if (current === end) {
-                    clearInterval(timer);
-                }
-            }, step);
+
+window.addEventListener('scroll', function() {
+    let hasFired = false;
+
+    if (document.body.scrollTop === 850) {
+        hasFired = true;
+        counter();
     }
-    counter("count1", 0, 5, 3000);
-    counter("count2", 0, 42, 2500);
-    counter("count3", 0, 63, 3000);
-    counter("count4", 0, 11, 3000)
 });
+
+function counter(id, start, end, duration) {
+    let obj = document.getElementById(id),
+        current = start,
+        range = end - start,
+        increment = end > start ? 1 : -1,
+        step = Math.abs(Math.floor(duration / range)),
+        timer = setInterval(() => {
+            current += increment;
+            obj.textContent = current;
+            if (current === end) {
+                clearInterval(timer);
+            }
+        }, step);
+}
+counter("count1", 0, 5, 3000);
+counter("count2", 0, 42, 2500);
+counter("count3", 0, 63, 3000);
+counter("count4", 0, 11, 3000);
